69e3863a8a1115a8a6b219476b9a3c0d53d50508
lachlan <lachlan.miller.1990@outlook.com>
2017-08-26 17:21:30 +0900

    Add config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
 	apps: [{
+		name: 'tutorial-2',
+		script: './lib/serverindex.js'
 	}],
 	deploy: {
 		production: {
 			user: 'ubuntu',
-			host: 'ec2-52-201-254-43.compute-1.amazonaws.com'
+			host: 'ec2-52-201-254-43.compute-1.amazonaws.com',
 			key: '~/.ssh/share-my-anything-production.pem',
 			ref: 'origin/master',
 			repo: 'git@github.com:lmiller1990/share-my-anything.git',
 			path: '/home/ubuntu/share-my-anything',
-			'post-deploy': 'yarn install && && yarn prod:build && yarn prod:start'
+			'post-deploy': 'npm install && npm prod:build pm2 startOrRestart ecosystem.config.js'
 		}
 	}
 }
