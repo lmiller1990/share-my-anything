c1cdc3f4e7493730da98a28be23ac49a61475af8
lachlan <lachlan.miller.1990@outlook.com>
2017-08-26 18:57:36 +0900

    Update config

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,5 +1,5 @@
 export const WDS_PORT = 7000
-export const WEB_PORT = process.env.PORT || 8000
+export const WEB_PORT = process.env.PORT || 3000
 
 export const STATIC_PATH = '/static'
 export const AWS_BUCKET_ROOT_URL = "https://s3.us-east-2.amazonaws.com/share-my-anything/"
