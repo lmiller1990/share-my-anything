96f6497abeade0e50bddcced499d6fd29e303ac3
lachlan <lachlan.miller.1990@outlook.com>
2017-09-09 13:46:56 +0900

    remove old route

--- src/client/router.js
+++ src/client/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Category from './components/Category.vue'
 import Home from './components/Home.vue'
 
 Vue.use(VueRouter)
@@ -10,14 +9,7 @@ const router = new VueRouter({
 	routes: [
 		{ 
 			path: '/', 
-			component: Home,
-			children: [
-				{
-					name: 'category',
-					path: '/:category',
-					component: Category
-				}
-			]
+			component: Home
 		}
 	]
 })
